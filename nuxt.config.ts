64a9510b7799e5685c641e5e5d018e2fc5d2858b
QC2168 <huangxinhong2125@gmail.com>
2022-05-11 18:00:11 +0800

    import tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,20 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        },
+},
+    css: [
+        '@/assets/css/main.css',
+      ],
     typescript: {
         strict: true
-      }
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
        
 <template>
   <div>
-    <NuxtLink to="/demo1">to Demo1.vue (NuxtLink)</NuxtLink>
+    <NuxtLink class="text-blue-500" to="/demo1">to Demo1.vue (NuxtLink)</NuxtLink>
     <h1>Index page</h1>
   </div>
 </template>
